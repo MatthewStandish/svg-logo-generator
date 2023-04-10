e186caa70c7d2b8cc10e13cc26f0d0e46013c443
Matthew Standish <mwstandish@aol.com>
2023-04-10 19:53:55 -0400

    getSVG and fs details

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Circle, Square } = require("./lib/shapes");
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message: "Enter up to three characters for the text:",
-    validate: (value) => {
-      if (value.length > 3) {
-        return "Text must be 3 characters or less.";
-      }
-      return true;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Enter up to three characters for the text:",
+      validate: (value) => {
+        if (value.length > 3) {
+          return "Text must be 3 characters or less.";
+        }
+        return true;
+      },
     },
-  },
-  {
-    type: "input",
-    name: "textColor",
-    message:
-      "Enter a color keyword (OR a hexadecimal number) for the text color:",
-  },
-  {
-    type: "list",
-    name: "shape",
-    message: "Choose a shape from the following options:",
-    choices: [
-      { name: "Triangle", value: new Triangle() },
-      { name: "Circle", value: new Circle() },
-      { name: "Square", value: new Square() },
-    ],
-  },
-  {
-    type: "input",
-    name: "shapeColor",
-    message:
-      "Enter a color keyword (OR a hexadecimal number) for the shape's color:",
-  },
-]);
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "Enter a color keyword (OR a hexadecimal number) for the text color:",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Choose a shape from the following options:",
+      choices: [
+        { name: "Triangle", value: new Triangle() },
+        { name: "Circle", value: new Circle() },
+        { name: "Square", value: new Square() },
+      ],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message:
+        "Enter a color keyword (OR a hexadecimal number) for the shape's color:",
+    },
+  ])
+  .then((answers) => {
+    const svg = answers.shape.getSVG(
+      answers.text,
+      answers.textColor,
+      answers.shapeColor
+    );
+    fs.writeFile("logo.svg", svg, (err) => {
+      if (err) throw err;
+      console.log("Generated logo.svg");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
